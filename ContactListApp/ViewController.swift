47e4f414e5d8f03b3b44be25c16720a58407ca0e
Gerardo <gerardo_mendoza95@hotmail.com>
2020-09-12 21:08:44 -0700

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Gerardo on 12/09/20.
+//  Copyright © 2020 Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
